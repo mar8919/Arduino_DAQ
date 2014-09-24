b97f32f1f61c22324d8c3a934a7e4cdc36e7e1a9
mar8919 <cmar8919@gmail.com>
2014-09-23 21:34:04 -0700

    Initial Working Files
    Folder Contents:
    AJAX_Server: Arduino Yun Sketch
    www: files for web server

---
+++ AJAX_Server/AJAX_Server.ino
@@ -0,0 +1,95 @@
+#include <FileIO.h>
+#include <Bridge.h>
+#include <YunServer.h>
+#include <YunClient.h>
+
+const int ledPin = 13; // the pin that the LED is attached to
+int incomingByte;      // a variable to read incoming serial data into
+YunServer server;
+
+void setup() {
+  // initialize communication:
+  Bridge.begin();
+  server.listenOnLocalhost();
+  server.begin();
+
+  
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  
+  
+  // initialize file
+  FileSystem.begin();
+  File output = FileSystem.open("/mnt/sd/arduino/www/output.csv", FILE_APPEND); 
+  //TODO: Make append for read system, Add delete button?
+
+  // test write to file
+  if(output)
+  {
+    output.println(String(millis()) + ",Sketch Starting");
+    // close test file
+    output.close();
+  }
+  else
+  {
+    digitalWrite(ledPin, HIGH);  //LED 13 turns on to indicate file error
+  }
+}
+
+
+
+void loop()
+{
+  // Get clients coming from server
+  YunClient client = server.accept();
+  
+  // Check for new client
+  if (client)
+  {
+    process(client);
+    
+    //end client connection
+    client.stop();
+  }
+  
+  //delay(10);
+}
+
+
+
+void process(YunClient client)
+{
+  String command = client.readStringUntil('/');  // Parse string for first section
+  if(command == "stop")
+  {
+    //TODO:
+    //preset to turn off all solonoids (except release?)
+  }
+  if(command == "digital")
+  {
+    int pin = client.parseInt();
+    if (client.read() == '/')
+    {
+      int value = client.parseInt();
+      digitalWrite(pin, value);
+      
+      //server response
+      client.print(pin);
+      client.print(",");
+      client.println(value);
+      
+      File output = FileSystem.open("/mnt/sd/arduino/www/output.csv", FILE_APPEND);
+      if(output)
+      {
+        output.println( String(millis()) + ",Pin " + String(pin) + " set to " + String(value) );
+        output.close();
+      }
+      else
+      {
+        digitalWrite(ledPin, HIGH);  //LED 13 turns on to indicate file error
+      }
+    }
+  }
+}
